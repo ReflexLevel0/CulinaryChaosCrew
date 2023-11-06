cb0f9aa6faaeba5c71ed6190002fc15b5219d591
Gl1tc6 <ante0908@gmail.com>
2023-11-06 13:32:10 +0100

    Added revised backend
    This one should provide ability to add,delete,edit,find and view all recepies in memory

---
+++ backend_revised/src/main/java/com/PROGI/backend/dao/RecipeDao.java
@@ -0,0 +1,27 @@
+package com.PROGI.backend.dao;
+
+import com.PROGI.backend.model.Recipe;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public interface RecipeDao {
+
+    int insertRecipe(UUID id, Recipe recipe);
+
+    default int insertRecipe(Recipe recipe){
+        UUID id = UUID.randomUUID();
+        return insertRecipe(id, recipe);
+    }
+
+    List<Recipe> selectAllRecipes();
+
+
+    Optional<Recipe> selectRecipeById(UUID id);
+    int deleteRecipeById(UUID id);
+
+    int updateRecipeById(UUID id, Recipe recipe);
+}
