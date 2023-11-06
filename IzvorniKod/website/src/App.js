f4b46891fb03444971471734d4f1018bb93ef220
ReflexLevel0 <dominikdejanovic2@gmail.com>
2023-11-06 19:25:52 +0100

    Added login view

--- IzvorniKod/website/src/App.js
+++ IzvorniKod/website/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import RecipeList from "./components/RecipeList";
+import LoginView from "./views/LoginView"
 
 function App() {
   return (<>
-    <RecipeList/>
+    <LoginView/>
   </>);
 }
 
---
+++ IzvorniKod/website/src/views/LoginView.js
@@ -0,0 +1,33 @@
+import '../styles/LoginViewDesktop.css'
+import {useState} from "react";
+
+function LoginView(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    return(
+        <div className="formWrapper">
+            <div/>
+            <div/>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="usernameInput">Username</label>
+                    <input type="text" className="form-control" id="usernameInput" aria-describedby="usernameHelp" placeholder="Enter username"
+                           minLength={6} required={true} value={username} onChange={e => setUsername(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="passwordInput">Password</label>
+                    <input type="password" className="form-control" id="passwordInput" placeholder="Password"
+                           minLength={6} required={true} value={password} onChange={e => setPassword(e.target.value)}/>
+                </div>
+                <div className="buttonWrapper">
+                    <button type="login" className="btn btn-primary" onClick={() => alert(`log in with ${username} ${password}`)}>Login</button>
+                    <button type="register" className="btn btn-primary" onClick={() => alert(`sign up with ${username} ${password}`)}>Sign up</button>
+                </div>
+            </form>
+            <div/>
+            <div/>
+        </div>
+    )
+}
+
+export default LoginView;
