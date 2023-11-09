ee2e7625789b809bee13dc9b6dae06fc713b4c51
ReflexLevel0 <dominikdejanovic2@gmail.com>
2023-11-09 23:22:46 +0100

    Added login form validation check

--- IzvorniKod/website/src/pages/Login.js
+++ IzvorniKod/website/src/pages/Login.js
@@ -1,38 +1,71 @@
 import '../styles/LoginViewDesktop.css'
-import {useState} from "react";
+import React from "react";
 
-function Login(){
-function Login({ login }){
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordRepeat, setPasswordRepeat] = useState("")
-    return(
-        <div className="formWrapper">
-            <div/>
-            <div/>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="usernameInput">Username</label>
-                    <input type="text" className="form-control" id="usernameInput" aria-describedby="usernameHelp" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="passwordInput">Password</label>
-                    <input type="password" id="passwordInput" className="form-control" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+export default class Login extends React.Component{
+    constructor(props) {
+        super(props)
+        this.props = props
+        this.state = {
+            username: "",
+            password: "",
+            passwordRepeat: ""
+        }
+    }
+
+    login(event, login){
+        let username = document.getElementById("usernameInput").value
+        let password = document.getElementById("passwordInput").value
+        let passwordRepeat = login ? null : document.getElementById("passwordRepeatInput").value
+        if(username.length === 0){
+            alert("Please enter username!")
+        }
+        else if(login === false && username.length < 6){
+            alert("Username should be at least 6 characters long!")
+        }
+        else if(password.length === 0){
+            alert("Please enter password!")
+        }
+        else if(login === false && password.length < 6){
+            alert("Password should be at least 6 characters long!")
+        }
+        else if(login === false && password !== passwordRepeat) {
+            alert("Passwords don't match!")
+        }else{
+            alert(`${login ? "log in" : "sign up" } with ${username} ${password}`)
+            return true
+        }
+        event.preventDefault()
+        return false
+    }
+
+    render(){
+        let login = this.props.login
+        return(
+            <div className="formWrapper">
+                <div/>
+                <div/>
+                <form onSubmit={e => this.login(e, login)}>
+                    <div className="form-group">
+                        <label htmlFor="usernameInput">Username</label>
+                        <input type="text" className="form-control" id="usernameInput" aria-describedby="usernameHelp" placeholder="Enter username" value={this.state.username} onChange={e => this.setState({username: e.target.value})}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="passwordInput">Password</label>
+                        <input type="password" id="passwordInput" className="form-control" placeholder="Password" value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
+                        {login ?
+                            (<></>) :
+                            (<input type="password" id="passwordRepeatInput" className="form-control" placeholder="Repeat password"
+                                    value={this.state.passwordRepeat} onChange={e => this.setState({passwordRepeat: e.target.value})} />)
+                        }
+                    </div>
                     {login ?
-                        (<></>) :
-                        (<input type="password" id="passwordRepeatInput" className="form-control" placeholder="Repeat password"
-                           value={passwordRepeat} onChange={e => setPasswordRepeat(e.target.value)} />)
+                        (<button type="submit" className="btn btn-primary">Login</button>) :
+                        (<button type="submit" className="btn btn-primary">Register</button>)
                     }
-                </div>
-                {login ?
-                    (<button type="submit" className="btn btn-primary">Login</button>) :
-                    (<button type="submit" className="btn btn-primary">Register</button>)
-                }
-            </form>
-            <div/>
-            <div/>
-        </div>
-    )
+                </form>
+                <div/>
+                <div/>
+            </div>
+        )
+    }
 }
-
-export default Login;
